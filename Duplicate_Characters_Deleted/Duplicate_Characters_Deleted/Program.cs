dc26d8a4f54565400471350253d9a4166d7aed0a
miyaluc <srlucas3@gmail.com>
2018-03-14 12:11:45 -0700

    set up method to remove duplicates

---
+++ Duplicate_Characters_Deleted/Duplicate_Characters_Deleted/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Duplicate_Characters_Deleted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Duplicate_Characters_Deleted/Duplicate_Characters_Deleted/Removal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duplicate_Characters_Deleted
+{
+    class Removal
+    {
+        public string RemoveDuplicates(string input)
+        {
+            string result = "";
+
+            foreach (char value in input)
+            {
+                if (result.IndexOf(value) == -1)
+                {
+                    result += value;
+                }
+            }
+            return result;
+        }
+    }
+}
